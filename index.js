45f1ec5fa5c2d9a512145d82e103520a2315b3a2
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-04-25 17:19:21 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const conectarDB = require("./config/db");
+
+// crear el servidor
+const app = express();
+
+// Conectar a la base de datos
+conectarDB();
+
+console.log("Comenzando Node Send");
+
+// Puerto de la app
+const port = process.env.PORT || 4000;
+
+// Rutas de la app
+app.use("/api/usuarios", require("./routes/usuarios"));
+
+// Arrancar la app
+app.listen(port, "0.0.0.0", () => {
+  console.log(`El servidor esta funcionando em el puerto ${port}`);
+});
