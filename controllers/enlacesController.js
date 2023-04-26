9d63ef17021b89bdfed570d1860d9b3112f883c5
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-04-26 16:55:23 -0300

    Definir o número de downloads e a senha para liberar o download

--- controllers/enlacesController.js
+++ controllers/enlacesController.js
@@ -1,17 +1,36 @@
 const Enlaces = require("../models/Enlace");
 const shortid = require("shortid");
+const bcrypt = require("bcrypt");
 
 exports.nuevoEnlace = async (req, res, next) => {
   // Revisar si hay errore
 
   // Crear un objeto de Enlace
-  const { nombre_original, password } = req.body;
+  const { nombre_original } = req.body;
 
   const enlace = new Enlaces();
   enlace.url = shortid.generate();
   enlace.nombre = shortid.generate();
   enlace.nombre_original = nombre_original;
-  enlace.password = password;
+
+  // Si el usuario está autenticado
+  if (req.usuario) {
+    const { password, descargas } = req.body;
+
+    // Asignar a enlace el número de descargas
+    if (descargas) {
+      enlace.descargas = descargas;
+    }
+
+    // asignar un password
+    if (password) {
+      const salt = await bcrypt.genSalt(10);
+      enlace.password = await bcrypt.hash(password, salt);
+    }
+
+    // Asignar el autor
+    enlace.autor = req.usuario.id;
+  }
 
   // Almacenar en la BD
   try {
