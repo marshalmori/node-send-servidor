c3fe8de423d554211720f75be58895b69c6c22e4
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-04-27 10:07:11 -0300

    Renomear e adicionar extensão aos arquivos

--- routes/archivos.js
+++ routes/archivos.js
@@ -3,11 +3,7 @@ const router = express.Router();
 const archivosController = require("../controllers/archivosController");
 const auth = require("../middleware/auth");
 
-// Subida de archivos
-const multer = require("multer");
-const upload = multer({ dest: "./uploads/" });
-
-router.post("/", upload.single("archivo"), archivosController.subirArchivo);
+router.post("/", auth, archivosController.subirArchivo);
 router.delete("/:id", archivosController.eliminarArchivo);
 
 module.exports = router;
